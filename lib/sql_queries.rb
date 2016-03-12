c80e2bb12e7ddc8a4e1f0717ba14f4e30535627e
Nathaniel Miller <dukeoflaser@gmail.com>
2016-03-12 21:55:25 +0000

    Write query strings to make tests pass

---
+++ lib/sql_queries.rb
@@ -0,0 +1,53 @@
+def selects_the_titles_of_all_projects_and_their_pledge_amounts
+"SELECT title, SUM(amount)
+ FROM projects
+ INNER JOIN pledges
+ ON projects.id = pledges.project_id
+ GROUP BY title
+ ORDER BY title ASC;"
+end
+
+def selects_the_user_name_age_and_pledge_amount_for_all_pledges
+"SELECT name, age, SUM(amount)
+ FROM users
+ INNER JOIN pledges
+ ON users.id = pledges.user_id
+ GROUP BY name
+ ORDER BY name ASC;"
+end
+
+def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
+"SELECT title, (SUM(amount) - funding_goal)
+ FROM projects
+ INNER JOIN pledges
+ ON projects.id = pledges.project_id
+ GROUP BY title
+ HAVING SUM(amount) >= funding_goal 
+ ORDER BY title ASC;
+"
+end
+
+def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount
+"SELECT name, SUM(amount)
+ FROM users
+ INNER JOIN pledges
+ ON users.id = pledges.user_id
+ GROUP BY name
+ ORDER BY SUM(amount) ASC;"
+end
+
+def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
+"SELECT category, amount
+ FROM projects
+ INNER JOIN pledges
+ ON projects.id = pledges.project_id
+ WHERE category = 'music';"
+end
+
+def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_book_category
+"SELECT category, SUM(amount)
+ FROM projects
+ INNER JOIN pledges
+ ON projects.id = pledges.project_id
+ WHERE category = 'books';"
+end
